1231d2e40d21612acca290c843b48a22a02d158f
张宏海 <1478527245@qq.com>
2018-11-06 20:28:49 +0800

    修改僵尸改变状态时对冰冻和吃大蒜的检测

--- Classes/EntityLayer.cpp
+++ Classes/EntityLayer.cpp
@@ -149,13 +149,22 @@ void EntityLayer::Check_isAttack_Zombie(float t)
 	for (int i = 0; i < readyZombies.size(); i++)
 	{
 		Zombie *zombie = readyZombies.at(i);
-		if (!zombie->hasHead()) continue;
+		bool jump = false;
+		if (!zombie->hasHead()) jump = true;
+		for (auto i : *(zombie->getDebuff())) {
+			if (i == Freezing || i == DrivingOut) {
+				jump = true;
+				break;
+			}
+		}
+		if (jump) continue;
 		Sprite *sp = zombie->getImg();
 		PlantStatus *p;
 		bool flag = false;
 		double zombiex = sp->getPositionX();
 		std::pair<int, int> zombie_rank = Point2Rank(sp->getPosition());
-		if (zombiex >= zombie_rank.first) {
+		double plantx = Rank2Point(zombie_rank.first, zombie_rank.second).x;
+		if (zombiex >= plantx) {
 			PlantStatus *ps = map::find(zombie_rank.first, zombie_rank.second);
 			if (ps != nullptr && ps->plantVector.size() > 0 && strcmp(typeid(*(ps->plantVector.at(0))).name(), "class Lucker") != 0){
 				flag = true;
