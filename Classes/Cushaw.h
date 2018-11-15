7a8047a5aa0aa3b9854c138559f28dbb74621744
sujianyu1997 <1040575641@qq.com>
2018-11-15 12:30:47 +0800

    优化植物种植登记函数

--- Classes/Cushaw.h
+++ Classes/Cushaw.h
@@ -3,8 +3,11 @@
 USING_NS_CC;
 class Cushaw :public Nut
 {
+private:
+	bool isProtecting = false;;
 public:
 	Cushaw(Point position, int row, int col);
+	void work();
 	//被打之后的事件
 	virtual void Attacked();
 
